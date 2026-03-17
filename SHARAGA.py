b5cf45d119e7dc10817704ab9006af83d95f6acc
Sashapr1200 <Sashapr1200@gmail.com>
2026-03-17 12:40:53 +0500

    Add logic for NFT generator. Closes #2

---
+++ SHARAGA.py
@@ -0,0 +1,15 @@
+var_int = 10
+var_float = 8.4
+var_str = "No"
+
+var_int = var_int * 3.5
+
+var_float = var_float - 1
+
+print("var_int / var_float =", var_int / var_float)
+
+var_str = var_str + var_str + "Yes" * 3
+
+print("var_int =", var_int)
+print("var_float =", var_float)
+print("var_str =", var_str)
